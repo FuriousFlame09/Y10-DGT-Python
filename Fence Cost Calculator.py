f6393b9f6b002c2f3ac05bb41c66171c60134726
MxvEn <89439149+FuriousFlame09@users.noreply.github.com>
2024-05-24 14:39:28 +1200

    Add files via upload

---
+++ Fence Cost Calculator.py
@@ -0,0 +1,7 @@
+# Fence cost calculator
+# V 1.0
+# Date: 24/05/24
+# Author: Byron Muller
+
+# Greets the user to the fence cost calculator
+print("Welcome to the fence cost calculator, are you ready to find the cost of your fence?")
